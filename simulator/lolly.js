d8872a1998514beffafd685599ca00a84de93328
Martin Bertschler <mbertschler@gmail.com>
2024-07-27 12:01:58 +0200

    improve FPS display

--- simulator/lolly.js
+++ simulator/lolly.js
@@ -1,6 +1,8 @@
 import { drawLolly } from "./draw.js";
 import { updateLedColors } from "./pattern.js";
 
+const fpsSelector = "#fps";
+
 const config = {
   ledCount: 63,
   radiusMm: 47,
@@ -14,24 +16,32 @@ const config = {
   },
 };
 
-let lastFrameTime = performance.now();
-
-function draw() {
+function drawFrame() {
   const ledColors = updateLedColors(config);
   drawLolly(config, ledColors);
 
-  requestAnimationFrame(draw);
-  updateFPS(performance.now());
+  requestAnimationFrame(drawFrame);
+  updateFPSDisplay();
 }
 
-requestAnimationFrame(draw);
+requestAnimationFrame(drawFrame);
 
-function updateFPS(currentTime) {
-  const timeDifference = currentTime - lastFrameTime;
-  const fps = 1000 / timeDifference;
-  lastFrameTime = currentTime;
+let lastFPSUpdate = performance.now();
+let frames = 0;
 
-  // Display FPS (you can log it, or update a DOM element)
-  //   console.log(fps.toFixed(2)); // for example, displaying to console
-  document.getElementById("fps").innerText = "FPS: " + fps.toFixed(1); // for example, updating a DOM element
+function updateFPSDisplay() {
+  frames++;
+
+  const now = performance.now();
+  if (now - lastFPSUpdate < 500) {
+    return;
+  }
+
+  const timeDiff = now - lastFPSUpdate;
+  const fps = 1000 / (timeDiff / frames);
+
+  lastFPSUpdate = now;
+  frames = 0;
+
+  document.querySelector(fpsSelector).innerText = "FPS: " + fps.toFixed(1);
 }
