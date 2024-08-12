ee3c2bff72ab70c1cb45f5ce8e11bd0631e374ed
Martin Bertschler <mbertschler@gmail.com>
2024-08-12 21:20:32 +0200

    add a WIFI AP with a web server that can switch between patterns

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -122,31 +122,6 @@ CRGB computeOneTwinkle(uint32_t ms, uint8_t salt);
 uint8_t attackDecayWave8(uint8_t i);
 void coolLikeIncandescent(CRGB &c, uint8_t phase);
 
-void setup()
-{
-    Serial.begin(115200);
-    Serial.println("initialized");
-
-    pinMode(LED_BOARD, OUTPUT);
-
-    for (int i = 0; i < NUM_BUTTONS; i++)
-    {
-        pinMode(buttonPins[i], INPUT_PULLUP);
-    }
-
-    // start with the LED off
-    digitalWrite(LED_BOARD, LOW);
-
-    delay(1000); // safety startup delay
-
-    FastLED.setBrightness(BRIGHTNESS);
-    // FastLED.setMaxPowerInVoltsAndMilliamps(VOLTS, MAX_MA);
-    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
-        .setCorrection(TypicalLEDStrip);
-
-    chooseNextColorPalette(gTargetPalette);
-}
-
 void loopTwinkleFox()
 {
     EVERY_N_SECONDS(SECONDS_PER_PALETTE)
@@ -568,6 +543,176 @@ bool checkButton(int buttonIndex)
 
 int patternIndex = 0;
 
+// ============================ WIFI code ============================
+
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com
+*********/
+
+// Load Wi-Fi library
+#include <WiFi.h>
+
+// Replace with your network credentials
+const char *ssid = "Mini Lolly";
+const char *password = "12345678";
+
+// Set web server port number to 80
+WiFiServer server(80);
+
+// Variable to store the HTTP request
+String header;
+
+void setupWifi()
+{
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Setting AP (Access Point)…");
+    // Remove the password parameter, if you want the AP (Access Point) to be open
+    WiFi.softAP(ssid, password);
+
+    IPAddress IP = WiFi.softAPIP();
+    // The network established by softAP will have default IP address of 192.168.4.1. This address may be changed using softAPConfig (see below).
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    server.begin();
+}
+
+void loopWifi()
+{
+    WiFiClient client = server.available(); // Listen for incoming clients
+
+    if (client)
+    { // If a new client connects,
+        unsigned long startMillis = millis();
+        // Serial.println("connected"); // print a message out in the serial port
+        String currentLine = ""; // make a String to hold incoming data from the client
+        while (client.connected())
+        { // loop while the client's connected
+            if (client.available())
+            {                           // if there's bytes to read from the client,
+                char c = client.read(); // read a byte, then
+                // Serial.write(c);        // print it out the serial monitor
+                header += c;
+                if (c == '\n')
+                { // if the byte is a newline character
+                    // if the current line is blank, you got two newline characters in a row.
+                    // that's the end of the client HTTP request, so send a response:
+                    if (currentLine.length() == 0)
+                    {
+                        // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+                        // and a content-type so the client knows what's coming, then a blank line:
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println("Connection: close");
+                        client.println();
+
+                        // turns the GPIOs on and off
+                        if (header.indexOf("GET /26/on") >= 0)
+                        {
+                            Serial.println("Twinkle Fox");
+                            patternIndex = 0;
+                        }
+                        else if (header.indexOf("GET /27/on") >= 0)
+                        {
+                            Serial.println("Rings");
+                            patternIndex = 1;
+                        }
+
+                        // Display the HTML web page
+                        client.println("<!DOCTYPE html><html>");
+                        client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+                        client.println("<link rel=\"icon\" href=\"data:,\">");
+                        // CSS to style the on/off buttons
+                        // Feel free to change the background-color and font-size attributes to fit your preferences
+                        client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+                        client.println(".button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;");
+                        client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+                        client.println(".button2 {background-color: #555555;}</style></head>");
+
+                        // Web Page Heading
+                        client.println("<body><h1>ESP32 Web Server</h1>");
+
+                        // Display current state, and ON/OFF buttons for GPIO 26
+                        client.println("<p>Twinkle Fox</p>");
+                        // If the output26State is off, it displays the ON button
+                        if (patternIndex == 0)
+                        {
+                            client.println("<p><a href=\"/26/on\"><button class=\"button\">ON</button></a></p>");
+                        }
+                        else
+                        {
+                            client.println("<p><a href=\"/26/on\"><button class=\"button button2\">ON</button></a></p>");
+                        }
+
+                        // Display current state, and ON/OFF buttons for GPIO 27
+                        client.println("<p>Rings</p>");
+                        // If the output27State is off, it displays the ON button
+                        if (patternIndex == 1)
+                        {
+                            client.println("<p><a href=\"/27/on\"><button class=\"button\">ON</button></a></p>");
+                        }
+                        else
+                        {
+                            client.println("<p><a href=\"/27/on\"><button class=\"button button2\">ON</button></a></p>");
+                        }
+                        client.println("</body></html>");
+
+                        // The HTTP response ends with another blank line
+                        client.println();
+                        // Break out of the while loop
+                        break;
+                    }
+                    else
+                    { // if you got a newline, then clear currentLine
+                        currentLine = "";
+                    }
+                }
+                else if (c != '\r')
+                {                     // if you got anything else but a carriage return character,
+                    currentLine += c; // add it to the end of the currentLine
+                }
+            }
+        }
+        // Clear the header variable
+        header = "";
+        // Close the connection
+        client.stop();
+        Serial.print("request took ");
+        Serial.print(millis() - startMillis);
+        Serial.println("ms");
+    }
+}
+
+// ============================ WIFI code ============================
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("initialized");
+
+    pinMode(LED_BOARD, OUTPUT);
+
+    for (int i = 0; i < NUM_BUTTONS; i++)
+    {
+        pinMode(buttonPins[i], INPUT_PULLUP);
+    }
+
+    // start with the LED off
+    digitalWrite(LED_BOARD, LOW);
+
+    delay(3000); // safety startup delay
+
+    setupWifi();
+
+    FastLED.setBrightness(BRIGHTNESS);
+    // FastLED.setMaxPowerInVoltsAndMilliamps(VOLTS, MAX_MA);
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS)
+        .setCorrection(TypicalLEDStrip);
+
+    chooseNextColorPalette(gTargetPalette);
+}
+
 void loop()
 {
     bool buttonPressed = checkButton(0);
@@ -589,4 +734,6 @@ void loop()
         loopRings();
         break;
     }
+
+    loopWifi();
 }
